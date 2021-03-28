f213315618e1e5b85938e8f8b88c7a0e274454c0
Gustav Cervera <gustavo.cervus@gmail.com>
2021-03-28 20:44:03 -0300

    update

--- _js/index.js
+++ _js/index.js
@@ -1,28 +1,22 @@
-const cam = document.getElementById("cam")
+const video = document.getElementById("cam")
 
-const startVideo = () => {
-    navigator.mediaDevices.enumerateDevices()
-    .then(devices => {
-        if (Array.isArray(devices)) {
-            //tem dispositivos ...
-            devices.forEach(device => {
-                if (device.kind === "videoinput") {
-                    console.log(device)
-                    if (device.label.includes("")) { //Etiqueta da câmera nativa está vazia em  sua string
-                        // tem a câmera certa...
-                        navigator.getUserMedia(
-                            {video: {
-                                deviceId: device.deviceId
-                            }},
-                            stream => cam.srcObject = stream,
-                            error => console.error(error)
-                        )
-                    }
-                }
-            })
-        }
-    })
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri("../lib/models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("../lib/models"),
+    faceapi.nets.tinyFaceDetector.loadFromUri("../lib/models"),
+    faceapi.nets.tinyFaceDetector.loadFromUri("../lib/models"),
+    faceapi.nets.tinyFaceDetector.loadFromUri("../lib/models")
+])
+
+function startVideo() {
+    navigator.getUserMedia(
+        {video: {} },
+        stream => cam.srcObjetct = stream,
+        err => console.log(err)
+    )
 }
+
+startVideo()
 
 
 
