90128e5f4e16bbd2a5305d622600ea82067d0c18
Gustav Cervera <gustavo.cervus@gmail.com>
2021-04-02 00:02:43 -0300

    update

--- _js/neural.js
+++
@@ -1,9 +0,0 @@
-
-Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('../lib/models'),
-    faceapi.nets.faceLandmar68Net.loadFromUri('../lib/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('../lib/models'),
-    faceapi.nets.ageGenderNet.loadFromUri('../lib/models'),
-    faceapi.nets.tinyFaceDetector.loadFromUri('../lib/models'),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('../lib/models')
-]).then(startVideo)
