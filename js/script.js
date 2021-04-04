321529028dc79144df2168b64d677a5725aef409
Gustav Cervera <gustavo.cervus@gmail.com>
2021-04-04 03:12:27 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function () {
+    var sidebar = document.querySelector(".container").classList.toggle("show-menu");
+});
+
